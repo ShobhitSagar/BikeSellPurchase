598004b46e7ffacd6e6fe4f53165a84cae7157ba
ShobhitSagar <shobhitsagar109@gmail.com>
2020-09-17 17:27:09 +0530

    Take a photo with a camera app.

--- app/src/main/java/com/example/bikesellpurchase/SellActivity.java
+++ app/src/main/java/com/example/bikesellpurchase/SellActivity.java
@@ -1,8 +1,13 @@
 package com.example.bikesellpurchase;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ImageView;
 
 public class SellActivity extends AppCompatActivity {
@@ -14,7 +19,24 @@ public class SellActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell);
 
+        mbuyerImage = findViewById(R.id.buyerImage);
 
+        mbuyerImage.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
 
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
 }
